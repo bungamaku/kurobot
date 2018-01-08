0af38f0fc54d3dca6b9a222419e13cfedb158529
Bunga Amalia <bungasokia@gmail.com>
2018-01-08 23:01:00 +0700

    Add deployment configuration

---
+++ Procfile
@@ -0,0 +1,2 @@
+migrate: bash deployment.sh
+web: gunicorn kurobot.wsgi --log-file -
---
+++ kurobot_line/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class KurobotLineConfig(AppConfig):
+    name = 'kurobot_line'
