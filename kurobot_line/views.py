23eaf047dd2489a21d9c83c0df1c31bf72dd94a2
Bunga Amalia <bungasokia@gmail.com>
2018-01-14 22:11:03 +0700

    Handle messaging API request

--- kurobot_line/views.py
+++ kurobot_line/views.py
@@ -14,28 +14,27 @@ parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
 
 @csrf_exempt
 def index(request):
-  if request.method == 'POST':
-      signature = request.META['HTTP_X_LINE_SIGNATURE']
-      body = request.body.decode('utf-8')
-
-      try:
-          events = parser.parse(body, signature)
-      except InvalidSignatureError:
-          return HttpResponseForbidden()
-      except LineBotApiError:
-          return HttpResponseBadRequest()
-      for event in events:
-          if isinstance(event, MessageEvent):
-              if isinstance(event.message, TextMessage):
-                  try:
-                      line_bot_api.reply_message(
-                          event.reply_token,
-                          TextSendMessage(text=event.message.text)
-                      )
-                  except LineBotApiError as e:
-                      print(e.status_code)
-                      print(e.error.message)
-                      print(e.error.details)
-  	  return HttpResponse()
-  else:
-      return HttpResponseBadRequest()
+	if request.method == 'POST':
+		signature = request.META['HTTP_X_LINE_SIGNATURE']
+		body = request.body.decode('utf-8')
+		try:
+			events = parser.parse(body, signature)
+		except InvalidSignatureError:
+			return HttpResponseForbidden()
+		except LineBotApiError:
+			return HttpResponseBadRequest()
+		for event in events:
+			if isinstance(event, MessageEvent):
+				if isinstance(event.message, TextMessage):
+					try:
+						line_bot_api.reply_message(
+						event.reply_token,
+						TextSendMessage(text=event.message.text)
+						)
+					except LineBotApiError as e:
+						print(e.status_code)
+						print(e.error.message)
+						print(e.error.details)
+		return HttpResponse()
+	else:
+		return HttpResponseBadRequest()
